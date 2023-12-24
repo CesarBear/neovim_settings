59a2998f2792894ccb16592a64567411a7d318ec
Youlun Peng <cesarslun51@gmail.com>
2023-12-24 19:42:17 +0100

    Modify plugins
    - add toggleterm for better experience of using terminal within neovim
    - toggle the new-tree filesystem with C-n
    - add lazygit to have awesome git

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,21 @@
-return  
+return
 {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left', {})
+      vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>', {})
+      require('neo-tree').setup({
+        popup_border_style = "rounded",
+        window = {
+          position = 'left',
+          width = 30,
+        }
+      })
     end
 }
 
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  'akinsho/toggleterm.nvim',
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<C-\>]],
+      size = 20,
+      hide_number = true,
+      direction = 'float',
+      shell = vim.o.shell,
+      shade_terminals = true,
+      shading_factor = 20
+    })
+  end
+}
